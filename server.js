2b811d16539c96ee49dcf519b7f16c3f339584ca
Napo <Michaeljnapolitano@gmail.com>
2020-10-15 22:01:40 -0400

    moved routes to server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-// const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// const apiRoutes = require('./routes/apiRoutes/index');
+// const htmlRoutes = require('./routes/htmlRoutes/index');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
+const notes = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const fs = require ('fs')
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +14,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
 // app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use('/', htmlRoutes);
+
+//  HTML ROUTE ------------
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
 
 
 
